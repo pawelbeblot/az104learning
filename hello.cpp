7a823e66ce43a329e746969a92c16928e5b9779c
Pawel Beblot <pawelbeblot@gmail.com>
2023-12-10 18:51:52 +0100

    full push

--- hello.cpp
+++ hello.cpp
@@ -3,8 +3,8 @@
 
 int main()
 {
-  const int windowWidth{1000};
-  const int windowHeight{1000};
+  const int windowWidth{500};
+  const int windowHeight{500};
   InitWindow(windowWidth, windowHeight, "Window");
   const int width(100);
   const int height(100);
